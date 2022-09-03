6acccab5c9fbba2f9dfb2ee9a522bd088c572f50
Axe <axearzadon@gmail.com>
2022-09-02 18:57:59 -0700

    Refactored app.js file in client folder to include apollo client

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,37 @@
+import { gql } from '@apollo/client';
+
+// Login Mutation
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// Add User Mutation
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          authors
+          bookId
+          image
+          link
+          title
+          description
+        }
+      }
+    }
+  }
+`;
